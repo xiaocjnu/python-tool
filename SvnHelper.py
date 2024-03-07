4c8efb5ede4b8839eb26e792b8c46838d2f733c5
axe <axe@xx.com>
2024-03-07 10:27:38 +0800

    feat:get_last_changed_date

--- SvnHelper.py
+++ SvnHelper.py
@@ -21,6 +21,29 @@ def get_revision(svndir):
             if line.startswith("Last Changed Rev: "):
                 return line.replace("Last Changed Rev: ", "").lstrip().rstrip()
         return ""
+    
+def get_last_changed_date(svndir):
+    """
+    获取目录下的最新一次提交日期
+    :param svndir:
+    :return:
+    """
+    with ChDir(svndir):
+        cmd = "svn info"
+        pipe = os.popen(cmd)
+        content = pipe.read()
+
+        lines = content.split("\n")
+        for line in lines:
+            if line.startswith("Last Changed Date: "):
+                line = line.split("(")[0]
+                return line.replace("Last Changed Date: ", "").lstrip().rstrip()
+        return ""
+    
+def svnupgrade(svndir):
+    with ChDir(svndir):
+        cmd = "svn upgrade"
+        os.popen(cmd)
 
 
 def get_rep_revision(svndir):
